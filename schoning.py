e82207990cc34a82ffc9ace87a959a0f0a8cd8dc
Jaival Patel <pateljaival05@gmail.com>
2022-04-22 09:54:40 -0400

    fetch all nums

--- schoning.py
+++ schoning.py
@@ -24,6 +24,8 @@ class Schoning:
         resulting_bits = self.string_to_bit(resulted_string)
         print(resulting_bits)
         self.bit_form = resulting_bits
+        fetched_results = self.fetch_numbers(self.total_string)
+        print(fetched_results)
 
 
     def get_string(self, bits, clauses):
@@ -64,6 +66,19 @@ class Schoning:
             bits.append(0)
     
         return bits
+
+    def fetch_numbers(self, strings):
+        lst_of_inputs = []
+        for lst in strings:
+            for number in lst:
+                number = int(number)
+                if not number in lst_of_inputs:
+                    lst_of_inputs.append(number)
+                else:
+                    continue 
+
+        return lst_of_inputs
+
 
 
 test = Schoning()
