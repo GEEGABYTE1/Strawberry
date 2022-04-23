a98eb46e48a9a35f109d872e14b7e6b15eb47455
Jaival Patel <pateljaival05@gmail.com>
2022-04-22 22:32:42 -0400

    result

--- schoning.py
+++ schoning.py
@@ -8,11 +8,19 @@ class Schoning:
 
     total_string = None     # Input and 
     bit_form = None         # Bits are Parallel
+    initial_string = None
 
     def experiment(self):
         self.get_input()
         resulted_string = self.get_string(self.num_bits, self.num_clauses)
+        self.initial_string = resulted_string
+        print('Resulting String:  ')
         print(resulted_string)
+        print('\n') 
+        print('Resulting Bits: ')
+        resulting_bits = self.result(resulted_string)
+        print(resulting_bits)
+
 
     def get_input(self):
         self.num_bits = int(input('Number of Bits: '))
@@ -43,7 +51,25 @@ class Schoning:
                     print('There aren not enough inputs')
 
         
-        return strings
+        return strings  
+    
+    def result(self, strings):
+        bits = []
+        for lst in strings:
+            binary_num = []
+            for dictionary in lst:
+                for number, state in dictionary.items():
+                    state = state.strip(' ')
+                    state = state.title()
+                    if state == 'True':
+                        binary_num.append(1)
+                    else:
+                        binary_num.append(0)
+            
+            bits.append(binary_num)
+        
+        return bits 
+            
 
 test = Schoning()
 print(test.experiment())
